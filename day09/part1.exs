66a3da91e6af7166f58d3b3fa98b22d59afe53b4
Johannes Opper <johannesopper@riseup.net>
2017-12-09 23:12:22 +0100

    Refactored day 9 part 1 - so much easier!

--- day09/part1.exs
+++ day09/part1.exs
@@ -1,44 +1,31 @@
 defmodule GarbageStream do
   def score(string) do
-    {transform, _} = string
-    |> String.trim
+    string
     |> String.graphemes
-    |> filter("")
-    |> String.replace(~r/\[,+/, "[")
-    |> Code.eval_string
-
-    GarbageStream.sum(transform, 1)
+    |> sum_depths(1)
   end
 
-  def filter([], groups) do
-    groups
+  def sum_depths([], _), do: 0
+
+  def sum_depths(["!", _ | tail], depth), do: sum_depths(tail, depth)
+
+  def sum_depths([head | tail], depth) do
+    case head do
+      "<" -> garbage(tail, depth)
+      "{" -> sum_depths(tail, depth + 1) + depth
+      "}" -> sum_depths(tail, depth - 1)
+      "," -> sum_depths(tail, depth)
+      _   -> sum_depths(tail, depth)
+    end
   end
 
-  def filter(["!" | tail], groups), do: ignore(tail, groups)
-  def filter(["<" | tail], groups), do: garbage(tail, groups)
-  def filter(["," | tail], groups), do: filter(tail, groups <> ",")
-  def filter(["{" | tail], groups), do: filter(tail, groups <> "[")
-  def filter(["}" | tail], groups), do: filter(tail, groups <> "]")
-  def filter([_ | tail], groups), do: filter(tail, groups)
+  def garbage(["!", _ | tail], depth), do: garbage(tail, depth)
 
-  def ignore([_ | tail], groups), do: filter(tail, groups)
-
-  def ignore_garbage([_ | tail], groups), do: garbage(tail, groups)
-
-  def garbage(["!" | tail], groups), do: ignore_garbage(tail, groups)
-  def garbage([">" | tail], groups), do: filter(tail, groups)
-  def garbage([_ | tail], groups), do: garbage(tail, groups)
-
-  def sum([], depth) do
-    depth
-  end
-
-  def sum(el, depth) when is_list(el) do
-    depth + Enum.reduce(el, 0, fn(l, t) -> sum(l, depth + 1) + t end)
-  end
-
-  def sum(_, _) do
-    0
+  def garbage([head | tail], depth) do
+    case head do
+      ">" -> sum_depths(tail, depth)
+      _   -> garbage(tail, depth)
+    end
   end
 end
 
@@ -47,12 +34,6 @@ ExUnit.configure trace: true
 
 defmodule GarbageStreamTest do
   use ExUnit.Case
-
-  test "sum []",        do: assert GarbageStream.sum([], 1) == 1
-  test "sum [\"a\"]",   do: assert GarbageStream.sum(["a"], 1) == 1
-  test "sum [[]]",      do: assert GarbageStream.sum([[]], 1) == 3
-  test "sum [[],[]]",   do: assert GarbageStream.sum([[],[]], 1) == 5
-  test "sum [[[]]]",    do: assert GarbageStream.sum([[[]]], 1) == 6
 
   test "score {}" do
     assert GarbageStream.score("{}") == 1
